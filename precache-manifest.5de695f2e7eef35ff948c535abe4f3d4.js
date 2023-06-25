c9fe7efdb59dc03f5cf9c9a02bd33a1d6cacb4d7
srikanth-css <115080562+srikanth-css@users.noreply.github.com>
2023-06-25 12:30:13 +0530

    Updates

---
+++ precache-manifest.5de695f2e7eef35ff948c535abe4f3d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1d02ca284f3c1e80a8f",
+    "url": "/Resume/static/css/main.6330d167.chunk.css"
+  },
+  {
+    "revision": "a1d02ca284f3c1e80a8f",
+    "url": "/Resume/static/js/main.a1d02ca2.chunk.js"
+  },
+  {
+    "revision": "8661c4af1aa50a4cec73",
+    "url": "/Resume/static/js/1.8661c4af.chunk.js"
+  },
+  {
+    "revision": "e9dd5708f2a8b3ef2344",
+    "url": "/Resume/static/js/2.e9dd5708.chunk.js"
+  },
+  {
+    "revision": "e044fd44a648b8f3224e",
+    "url": "/Resume/static/js/runtime~main.e044fd44.js"
+  },
+  {
+    "revision": "ee24c7247efdf4b23bf46808012dab69",
+    "url": "/Resume/static/media/th.ee24c724.jpg"
+  },
+  {
+    "revision": "e3a641a3a35e0cd34720aa4c82815587",
+    "url": "/Resume/static/media/1.e3a641a3.jpeg"
+  },
+  {
+    "revision": "09bfaeca9df160e85ec8818f5fb0283e",
+    "url": "/Resume/index.html"
+  }
+];
